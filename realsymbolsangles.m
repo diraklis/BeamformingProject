04da113c8efd07a66951ffd7bf56c489307ef862
diraklis <hraklis2002@hotmail.com>
2024-05-12 23:42:00 +0300

    Add files via upload

---
+++ realsymbolsangles.m
@@ -0,0 +1,18 @@
+function theta = realsymbolsangles(d,theta_0,realSymbols)
+
+
+
+theta=zeros(realSymbols,realSymbols);
+theta_test=zeros(1,realSymbols);
+
+for j=1:realSymbols
+for i=1:realSymbols
+    theta_test(1,i)=theta_0+(i-1)*d;    
+end
+
+theta(j,1:realSymbols)=circshift(theta_test(1,1:realSymbols),realSymbols-j+1);
+theta(j,2:realSymbols)=sort(theta(j,2:realSymbols));
+end
+
+end
+
