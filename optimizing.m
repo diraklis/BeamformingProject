4fd99b8977414169d284717023ec1975b05f4e45
diraklis <hraklis2002@hotmail.com>
2024-05-12 23:54:22 +0300

    optimizing.m

--- optimizing.m
+++ optimizing.m
@@ -4,14 +4,6 @@ function [sidelobes1,angleofzeros1,AF1,A11,w_nsb11] = optimizing(atheta,theta,th
 
     while (max(sidelobes)>0.1 || min(abs(angleofzeros(1,1:5)-theta_zeros(o,1:5)))>15)
         
-        
-    
-        for k=1:elements
-        for i=1:length(theta1)
-            A1(k,i)=exp(1j*(k-1)*bd*cos(theta1(i)*pi/180));
-        end
-        end
-        
         w_nsb=A1*inv( (A1')*A1+Pn*diag(ones(1,length(theta1))) );
         w_nsb1=w_nsb(1:elements,1);
         AF(1,1:length(th))=(w_nsb1')*atheta;
@@ -22,21 +14,28 @@ function [sidelobes1,angleofzeros1,AF1,A11,w_nsb11] = optimizing(atheta,theta,th
 
         th_peaks=P/10;
         theta1=th_peaks;
+        
         while theta1(1)>theta(o,1)+5 || theta1(1)<theta(o,1)-5
             theta1=circshift(theta1,-1);
         end
+        
         theta1(2:6)=sort(theta1(2:6));
-
+        
+        for k=1:elements
+            for i=1:length(theta1)
+                A1(k,i)=exp(1j*(k-1)*bd*cos(theta1(i)*pi/180));
+            end
+        end
         sidelobes=Maximums(1:end-1);
         PointsofZeros=islocalmin(abs(AF( 1, 1:end )));
         mins=x(PointsofZeros)/10;
         angleofzeros=findzeros(d,theta_zeros,mins,o);
-
+        
     end
     
     sidelobes1=sidelobes;
     angleofzeros1=angleofzeros;
-%     theta11=theta1;
+%   theta11=theta1;
     AF1=AF;
     A11=A1;
     w_nsb11=w_nsb1;
